a6d6d91782db788371de6873e2f3cacb0dfe69f9
varorbc <varorbc@gmail.com>
2017-11-28 14:39:16 +0800

    修改最低版本为461

--- test/ICanPay.UnitTest(Net)/Properties/AssemblyInfo.cs
+++ test/ICanPay.UnitTest(Net)/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ICanPay.UnitTest(.Net)")]
+[assembly: AssemblyTitle("ICanPay.UnitTest(Net)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ICanPay.UnitTest(.Net)")]
+[assembly: AssemblyProduct("ICanPay.UnitTest(Net)")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("28aa717d-d840-4c92-a2ad-afbf3794d6ef")]
+[assembly: Guid("9456f7d3-fdc2-4447-9e88-d97f542f5039")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
