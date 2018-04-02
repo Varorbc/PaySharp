2a29f564a68fa955932c32094b6e224bafe00f05
varorbc <varorbc@gmail.com>
2018-04-02 21:18:51 +0800

    添加APP支付

---
+++ src/ICanPay.Wechatpay/Domain/AppPayModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ICanPay.Wechatpay.Domain
+{
+    public class AppPayModel : BasePayModel
+    {
+        public AppPayModel()
+            : base("APP")
+        {
+
+        }
+
+        /// <summary>
+        /// 终端(用户端)IP
+        /// </summary>
+        [Required(ErrorMessage = "请设置终端IP")]
+        [StringLength(16, ErrorMessage = "终端IP最大长度为16位")]
+        public string SpbillCreateIp { get; set; }
+    }
+}
---
+++ src/ICanPay.Wechatpay/Request/AppPayRequest.cs
@@ -0,0 +1,13 @@
+﻿using ICanPay.Wechatpay.Domain;
+using ICanPay.Wechatpay.Response;
+
+namespace ICanPay.Wechatpay.Request
+{
+    public class AppPayRequest : BaseRequest<AppPayModel, AppPayResponse>
+    {
+        public AppPayRequest()
+        {
+            RequestUrl = "/pay/unifiedorder";
+        }
+    }
+}
